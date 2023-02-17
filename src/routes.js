ad984e93399f7f29340b60db9646c7bbdf926a9d
lucca <luccavilasboas@hotmail.com>
2023-02-17 18:44:19 -0300

    Biblioteca de livros back end

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+const { Router } = require('express')
+const routes = Router()
+const clientController = require('./modules/Cliente/ClienteControler')
+const livroController = require('./modules/Livro/LivroControler')
+const aluguelController = require('./modules/Aluguel/AluguelControler')
+
+
+routes.get('/', (req, res) => {
+    return res.json('Server up!')
+})
+
+routes.get('/cliente', clientController.get)
+routes.post('/cliente', clientController.create)
+
+routes.get('/livro', livroController.get)
+routes.post('/livro', livroController.create)
+routes.post('/livro/:id', livroController.putIsAlterado)
+
+routes.get('/aluguel', aluguelController.get)
+routes.post('/aluguel', aluguelController.create)
+routes.put('/aluguel', aluguelController.put)
+
+module.exports = { routes }
---
+++ tets.js
@@ -0,0 +1,202 @@
+<div class="row">
+    <div class="box__main">
+        <div class="container">
+            <div class="row">
+
+                <div class="main__item col-sm-3">
+                    <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/124434/para-que-serve-o-spotway">
+                        <i class="fas fa-road"></i>
+                        <h3>Base de conhecimento rodovias</h3>
+                    </a>
+                </div>
+                <div class="main__item col-sm-3">
+                    <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/118714/para-que-serve">
+                        <i class="fas fa-bolt-lightning"></i>
+                        <h3>Base de conhecimento energia</h3>
+                    </a>
+                </div>
+                <div class="main__item col-sm-3">
+                    <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/118714/para-que-serve">
+                        <i class="fa-solid fa-bolt-lightning"></i>
+                        <h3>Perguntas Frequentes</h3>
+                    </a>
+                </div>
+                <div class="main__item col-sm-3">
+                    <a class="flex-column align-center justify-content align-items" href="https://www.kartado.com.br/edu-kartado" _blank">
+                    <i class="icon-mv-wifi" aria-hidden="true"></i>
+                    <h3>Edu.Kartado</h3>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+</div >
+
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://www.movidesk.com/basedeconhecimento/style.min.css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+
+
+                <i class="fa-solid fa-road"></i>
+
+                <!-- INÍCIO - CSS PARA ALTERAÇÃO DE CORES -->
+                <style>
+                    .box__main .main__item a i,
+                    footer .box__footer .footer__contact i,
+body > .body-content > div.row aside .kb-menu-container li a.active,
+body > .body-content article#kb-article .article-content a,
+body > .body-content article#kb-article #rating-box-confirm h5 {
+                        color: #012d6d;
+}
+                    #header__top .enter__contact a.btn-entrar:hover,
+                    .search-container .search-item a h4:hover,
+body > .body-content > div.row aside .kb-menu-container li a:hover,
+body > .body-content article#kb-article .tags-info a:hover,
+body > .body-content article#kb-article .article-footer #attachments ul li a:hover {
+                        color: #012d6d;
+}
+                    .header__search,
+                    footer .box__footer .footer__social ul li a {
+                        background - color: #012d6d;
+}
+                    footer .box__footer .footer__social ul li a:hover {
+                        background - color: #005dff;
+}
+                </style>
+                <!-- FIM - CSS PARA ALTERAÇÃO DE CORES -->
+
+                <!-- Global site tag (gtag.js) - Google Analytics -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-66717520-9"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-66717520-9');
+                </script>
+
+                img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+
+
+
+
+
+
+
+
+                <div class="row">
+                    <div class="box__main">
+                        <div class="container">
+                            <div class="row">
+                                <div class="main__item col-sm-3">
+                                    <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/124434/para-que-serve-o-spotway">
+                                        <i class="fas fa-road"></i>
+                                        <h3>Base de conhecimento rodovias</h3>
+                                    </a>
+                                </div>
+                                <div class="main__item col-sm-3">
+                                    <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/118714/para-que-serve">
+                                        <i class="fas fa-bolt"></i>
+                                        <h3>Base de conhecimento energia</h3>
+                                    </a>
+                                </div>
+                                <div class="main__item col-sm-3">
+                                    <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/118714/para-que-serve">
+                                        <i class="icon-mv-question" aria-hidden="true"></i>
+                                        <h3>Perguntas Frequentes</h3>
+                                    </a>
+                                </div>
+                                <div class="main__item col-sm-3">
+                                    <a class="flex-column align-center justify-content align-items" href="https://www.kartado.com.br/edu-kartado" _blank">
+                                    <i class="icon-mv-wifi" aria-hidden="true"></i>
+                                    <h3>Edu.Kartado</h3>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="row">
+                <div class="box__main">
+                    <div class="container">
+                        <div class="row">
+                            <div class="main__item col-sm-3">
+                                <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/124434/para-que-serve-o-spotway">
+                                    <i class="fas fa-road"></i>
+                                    <h3>Base de conhecimento rodovias</h3>
+                                </a>
+                            </div>
+                            <div class="main__item col-sm-3">
+                                <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/118714/para-que-serve">
+                                    <i class="fas fa-bolt"></i>
+                                    <h3>Base de conhecimento energia</h3>
+                                </a>
+                            </div>
+                            <div class="main__item col-sm-3">
+                                <a class="flex-column align-center justify-content align-items" href="https://suporte.kartado.com.br/kb/pt-br/article/118714/para-que-serve">
+                                    <i class="icon-mv-question" aria-hidden="true"></i>
+                                    <h3>Perguntas Frequentes</h3>
+                                </a>
+                            </div>
+                            <div class="main__item col-sm-3">
+                                <a class="flex-column align-center justify-content align-items" href="https://www.kartado.com.br/edu-kartado" _blank">
+                                <i class="icon-mv-wifi" aria-hidden="true"></i>
+                                <h3>Edu.Kartado</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="row">
+            <div class="box__footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="footer__contact">
+                            <i class="icon-mv-support" aria-hidden="true"></i>
+                            <div class="contact">
+                                <b>Atendimento de suporte</b><br>
+                                    E-mail: suporte@kartado.com.br<br>
+                                        WhatsApp: +55 47 98416-4258
+                                    </div>
+                            </div>
+                            <div class="footer__social">
+                                <ul>
+                                    <li>
+                                        <a href="https://www.facebook.com/kartado.com.br" target="blank">
+                                            <i class="fa fa-facebook-f"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.linkedin.com/company/kartado/" target="blank">
+                                            <i class="fa fa-linkedin"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.instagram.com/kartado.com.br/" target="blank">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
